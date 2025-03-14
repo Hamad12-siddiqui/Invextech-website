3518293825c559e2350a7077719142bf0058744e
Hamad <hamadahamd7765@gmail.com>
2025-03-14 16:20:19 +0500

    website

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+    swcMinify: false, // Disable SWC to avoid platform issues
+  };
+  
+  module.exports = nextConfig;
+  
