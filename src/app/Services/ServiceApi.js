5ef7febbd1ca6e6a09f6f042d6d8d67e2b235c30
Hamad <hamadahamd7765@gmail.com>
2025-03-15 15:55:40 +0500

    bg img update

--- src/app/Services/ServiceApi.js
+++ src/app/Services/ServiceApi.js
@@ -9,35 +9,35 @@ const ServiceApi = [
     {
         id: 2,
         image1: "/images/Mobile App Development.png",
-        image2: "/images/vector.png",
+        image2: "/images/bgcircle.png",
         h2: "Mobile Development",
         p : "Whether build a native mobile app for iOS or Android, or a cross-platform solution using React Native, we have the skills and experience to bring your vision to life."
     },
     {
         id: 3,
         image1: "/images/brain.png",
-        image2: "/images/vector.png",
+        image2: "/images/bgcircle.png",
         h2: "AI Development Services",
 p : "Leverage AI to automate processes and enhance decision-making with BrainX. Our services include NLP, chatbot development, intelligent automation, and predictive analytics." 
     },
     {
         id: 4,
         image1: "/images/cart.png",
-        image2: "/images/vector.png",
+        image2: "/images/bgcircle.png",
         h2: "Ecommerce Development",
      p : "Starting or growing your online business? We have ecommerce experts for starting or scaling online stores, specializing in Shopify, WooCommerce, Magento and custom builds"
     },
     {
         id: 5,
         image1: "/images/comp.png",
-        image2: "/images/vector.png",
+        image2: "/images/bgcircle.png",
         h2: "Blockchain Development",
 p : "Interested in using blockchain technology to solve real-world problems? Our team of blockchain developers can help you build decentralized applications, smart contracts, and more."
     },
     {
         id: 6,
         image1: "/images/cloud.png",
-        image2: "/images/vector.png",
+        image2: "/images/bgcircle.png",
         h2: "DevOps Services",
 p : "Optimize your software development and deployment processes with our DevOps services. We provide tailored tool and platform selection, streamlining operations for optimal productivity"
     }
