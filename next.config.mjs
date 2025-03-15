973f1bf6b60196b22f39df84c45ddf5b52912910
Hamad <hamadahamd7765@gmail.com>
2025-03-15 13:02:51 +0500

    new invextech website

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-      swcLoader: true, // Ensure SWC is enabled
-      swcMinify: true, // Enable SWC minification
-    },
-    webpack: (config) => {
-      // Ensure cross-platform compatibility
-      config.resolve.fallback = { fs: false };
-      return config;
-    },
-  };
-  
-  export default nextConfig;
+const nextConfig = {};
+export default nextConfig;
   
