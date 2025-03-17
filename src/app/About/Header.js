fe2872895d294f49ee6830d41a97a85c34b99bac
Hamad <hamadahamd7765@gmail.com>
2025-03-17 15:20:28 +0500

    chnges in about

--- src/app/About/Header.js
+++ src/app/About/Header.js
@@ -20,15 +20,22 @@ const Header = () => {
           className="absolute inset-0 z-0 object-cover"
           priority
         />
-        <div className="relative z-10 flex h-full flex-col justify-center text-white max-w-[90%] md:max-w-[85%] mx-auto px-4">
-          <p className="underline text-sm md:text-base">About Us</p>
-          <h1 className="text-2xl md:text-4xl font-bold mt-2">Revolutionizing</h1>
-          <h1 className="text-2xl md:text-4xl font-bold mt-2">Businesses with Cutting-</h1>
-          <h2 className="text-2xl md:text-4xl font-bold mb-3">Edge Digital Innovations</h2>
-          <p className="text-sm md:text-base lg:text-lg">
-            We transform ideas into disruptive digital products and strategies to drive growth for Startups, SMBs, and Enterprises.
+        <div className="relative z-10 flex h-full flex-col justify-center text-white max-w-[90%] md:max-w-[85%] mx-auto px-4 sm:px-6 lg:px-8">
+          <p className="underline text-sm sm:text-base md:text-lg">About Us</p>
+          <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mt-2">
+            Revolutionizing
+          </h1>
+          <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mt-2">
+            Businesses with Cutting-
+          </h1>
+          <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-3">
+            Edge Digital Innovations
+          </h2>
+          <p className="text-sm sm:text-base md:text-lg lg:text-xl sm:max-w-[60%] md:max-w-[50%] lg:max-w-[52%] w-full">
+            We transform ideas into disruptive digital products and strategies
+            to drive growth for Startups, SMBs, and Enterprises.
           </p>
-          <button className="text-white bg-sky-600 mt-8 rounded-full py-2 px-4 md:py-3 md:px-6 max-w-[175px] md:max-w-[200px] hover:bg-sky-500">
+          <button className="text-white bg-sky-600 mt-8 rounded-full py-2 px-4 sm:py-3 sm:px-6 md:py-4 md:px-8 max-w-[175px] sm:max-w-[200px] md:max-w-[225px] hover:bg-sky-500 transition-all duration-300">
             Talk To Our Experts
           </button>
         </div>
